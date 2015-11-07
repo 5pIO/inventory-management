6007af302eccd9d2b4daf51e0f5647f297120653
Shrikant Patnaik <shrikant.patnaik@gmail.com>
2015-11-07 21:51:19 +0800

    Make categories paginated and filterable

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
   # GET /categories
   # GET /categories.json
   def index
-    @categories = Category.all
+    @categories_grid = initialize_grid(Category)
   end
 
   # GET /categories/1
